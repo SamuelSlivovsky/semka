8888e8b748e054452c2a5bd024288f39d061c622
Bl0ccky <tomas.labat1@gmail.com>
2022-11-19 15:07:51 +0100

    Update datoveho modelu

--- client/src/Views/Patient.jsx
+++ client/src/Views/Patient.jsx
@@ -1,6 +1,32 @@
 import React from "react";
+import { Card } from "primereact/card";
 
 function Patient() {
-  return <p>ddd</p>;
+  return (
+    <div>
+      <div className="col-6">
+        <Card
+          title="Simple Card"
+          style={{ width: "25rem", marginBottom: "2em" }}
+        >
+          <p className="m-0" style={{ lineHeight: "1.5" }}>
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Inventore
+            sed consequuntur error repudiandae numquam deserunt quisquam
+            repellat libero asperiores earum nam nobis, culpa ratione quam
+            perferendis esse, cupiditate neque quas!
+          </p>
+        </Card>
+
+        <Card title="Recepty" style={{ width: "25em" }}>
+          <p className="m-0" style={{ lineHeight: "1.5" }}>
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Inventore
+            sed consequuntur error repudiandae numquam deserunt quisquam
+            repellat libero asperiores earum nam nobis, culpa ratione quam
+            perferendis esse, cupiditate neque quas!
+          </p>
+        </Card>
+      </div>
+    </div>
+  );
 }
 export default Patient;
