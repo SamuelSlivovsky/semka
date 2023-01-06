97acbdade4c43db7148b83f3bfeccc34d8540b65
Marek Šútora <marek.sutora@gmail.com>
2023-01-06 19:48:49 +0100

    ReceptController converted to AddController

--- server/routes/addRoute.js
+++ server/routes/addRoute.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router()
-const controller = require("../controllers/ReceptController")
+const controller = require("../controllers/AddController")
 
 router.post("/recept", controller.insertRecept);
 
