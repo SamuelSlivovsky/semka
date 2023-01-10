a399e57cd1b33fa7d45926ab36d73beff606d108
Marek Šútora <marek.sutora@gmail.com>
2023-01-11 00:44:13 +0100

    fix

--- server/controllers/PatientController.js
+++ server/controllers/PatientController.js
@@ -5,7 +5,7 @@ module.exports = {
     (async () => {
       ret_val = await pacient.getInfo(req.params.id);
       res.status(200).json(ret_val);
-    })()().catch((err) => {
+    })().catch((err) => {
       console.error(err);
       res.status(403).send(err);
     });;
