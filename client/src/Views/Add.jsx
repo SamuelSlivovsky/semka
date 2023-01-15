b652c0c461c667e457b6bc38545f76198d6c8d9b
Bl0ccky <tomas.labat1@gmail.com>
2023-01-15 19:56:50 +0100

    Pridanie marekovho suboru

--- client/src/Views/Add.jsx
+++ client/src/Views/Add.jsx
@@ -1,34 +1,34 @@
-import React, { useState, useRef } from 'react';
-import { SelectButton } from 'primereact/selectbutton';
-import { Toast } from 'primereact/toast';
-import PatientForm from '../Forms/PatientForm';
-import RecipeForm from '../Forms/RecipeForm';
-import HospitForm from '../Forms/HospitForm';
-import OperationForm from '../Forms/OperationForm';
-import ExaminationForm from '../Forms/ExaminationForm';
+import React, { useState, useRef } from "react";
+import { SelectButton } from "primereact/selectbutton";
+import { Toast } from "primereact/toast";
+import PatientForm from "../Forms/PatientForm";
+import RecipeForm from "../Forms/RecipeForm";
+import HospitForm from "../Forms/HospitForm";
+import OperationForm from "../Forms/OperationForm";
+import ExaminationForm from "../Forms/ExaminationForm";
 export default function Add() {
   const toast = useRef(null);
   const [eventTypeButton, setEventTypeButton] = useState(1);
   const eventTypes = [
-    { name: 'Operácia', code: 'OP', value: 1 },
-    { name: 'Vyšetrenie', code: 'EX', value: 2 },
-    { name: 'Hospitalizácia', code: 'HOSP', value: 3 },
-    { name: 'Recept', code: 'RECEPT', value: 4 },
-    { name: 'Pacient', code: 'PAT', value: 5 },
+    { name: "Operácia", code: "OP", value: 1 },
+    { name: "Vyšetrenie", code: "EX", value: 2 },
+    { name: "Hospitalizácia", code: "HOSP", value: 3 },
+    { name: "Recept", code: "RECEPT", value: 4 },
+    { name: "Pacient", code: "PAT", value: 5 },
   ];
 
   return (
     <div
-      style={{ width: '90%', marginTop: '2rem' }}
-      className='p-fluid grid formgrid'
+      style={{ width: "90%", marginTop: "2rem" }}
+      className="p-fluid grid formgrid"
     >
       <Toast ref={toast} />
-      <div className='field col-12'>
+      <div className="field col-12">
         <SelectButton
           value={eventTypeButton}
           options={eventTypes}
           onChange={(e) => setEventTypeButton(e.value)}
-          optionLabel='name'
+          optionLabel="name"
         />
       </div>
       {eventTypeButton === 1 ? (
