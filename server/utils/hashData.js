fda52d82fff8d0bb56bfa5dd06382cd83a40f189
Jakub Rapsik <jrapsik@gmail.com>
2023-11-19 16:57:36 +0100

    Dokonceny HashPacienti

--- server/utils/hashData.js
+++ server/utils/hashData.js
@@ -1,14 +1,41 @@
 //TODO Doplnit funkcie zamienania udajov
 const fs = require('fs');
 
-function zistiPohlavie(rodneCislo) {
-    //TODO pridat podmeniku aby kontrolovalo rodne cislo cudzinca cudzinca
-    const pohlavieKod = rodneCislo[2];
-    // Ak je pohlavie kód párne číslo, ide o ženu, inak o muža
-    return pohlavieKod % 2 === 0 ? 'zena' : 'muz';
+function generujRodneCislo(pohlavie) {
+    let rok = Math.floor(Math.random() * (1999 - 1940 + 1)) + 1940;
+    rok = rok % 100;
+    const mesiac = Math.floor(Math.random() * 12) + 1;
+    const den = Math.floor(Math.random() * 28) + 1;
+
+    // Kód pohlavia (muž: 0-1, žena: 5-6) z tretiej číslice
+    let formatovanyMesiac
+    if (pohlavie === 'muz') {
+        formatovanyMesiac = (mesiac < 10) ? '0' + mesiac : mesiac;
+    } else {
+        formatovanyMesiac = '5' + mesiac;
+    }
+
+    // Pridanie nuly pred číslice menšie ako 10
+    const formatovanyDen = den < 10 ? '0' + den : den;
+    rok = rok < 10 ? '0' + rok : rok;
+
+    // Generovanie kontrolného kódu (4-miestne číslo)
+    const kontrolnyKod = Math.floor(Math.random() * 10000).toString().padStart(4, '0');
+
+    // Spojenie všetkých častí do rodného čísla
+    const rodneCislo = `${rok}${formatovanyMesiac}${formatovanyDen}/${kontrolnyKod}`;
+
+    return rodneCislo;
 }
 
-function nacitajMenaZoSuboru(nazovSuboru) {
+function zistiPohlavie(rodneCislo) {
+    //TODO pridat podmeniku aby kontrolovalo rodne cislo cudzinca cudzinca
+    const pohlavieKod = parseInt(rodneCislo.charAt(2));
+    // Ak je pohlavie kód párne číslo, ide o ženu, inak o muža
+    return pohlavieKod === 0 || pohlavieKod === 1 ? 'muz' : pohlavieKod === 5 || pohlavieKod === 6 ? 'zena' : null;
+}
+
+function nacitajDataZoSuboru(nazovSuboru) {
     try {
         const obsahSuboru = fs.readFileSync(`${nazovSuboru}`, 'utf-8');
         //Oddelovac v subore medzi menami
@@ -20,21 +47,38 @@ function nacitajMenaZoSuboru(nazovSuboru) {
 }
 
 function hashPacienti(pacienti) {
-    const mena = nacitajMenaZoSuboru("../server/utils/menaMuzy.txt"); // Príklad názvu súboru
+    const menaMuzy = nacitajDataZoSuboru("../server/utils/menaMuzy.txt");
+    const menaZeny = nacitajDataZoSuboru("../server/utils/menaZeny.txt");
+    const priezviskoMuzy = nacitajDataZoSuboru("../server/utils/priezviskoMuzy.txt")
+    const priezviskoZeny = nacitajDataZoSuboru("../server/utils/priezviskoZeny.txt")
 
-    if (mena.length === 0) {
+    if (menaMuzy.length === 0 || menaZeny === 0) {
         console.error('Nepodarilo sa načítať mená zo súboru.');
         return pacienti;
     }
     // Zmena mien v pacientoch
     const hashovaniPacienti = pacienti.map(pacient => {
         const pohlavie = zistiPohlavie(pacient.ROD_CISLO);
-        const indexMena = pohlavie === 'muz' ? Math.floor(Math.random() * 250) : Math.floor(Math.random() * 250) + 250; // Náhodný výber mena
-        const noveMeno = mena[indexMena];
+        // Náhodný výber Udajov
+        let indexMena;
+        let noveMeno;
+        let indexPriezviska;
+        let novepriezvisko;
+        if (pohlavie === 'muz') {
+            indexMena = Math.floor(Math.random() * menaMuzy.length);
+            indexPriezviska = Math.floor(Math.random() * priezviskoMuzy.length);
+            noveMeno = menaMuzy[indexMena];
+            novepriezvisko = priezviskoMuzy[indexPriezviska]
+        } else {
+            indexMena = Math.floor(Math.random() * menaZeny.length);
+            indexPriezviska = Math.floor(Math.random() * priezviskoZeny.length);
+            noveMeno = menaZeny[indexMena];
+            novepriezvisko = priezviskoZeny[indexPriezviska]
+        }
         return {
             MENO: noveMeno,
-            ROD_CISLO: pacient.ROD_CISLO,
-            PRIEZVISKO: pacient.PRIEZVISKO,
+            ROD_CISLO: generujRodneCislo(pohlavie),
+            PRIEZVISKO: novepriezvisko,
             PSC: pacient.PSC,
             ID_PACIENTA: pacient.ID_PACIENTA
         };
