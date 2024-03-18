52738b3e35a716379a0b32546297db1879ecae32
RobertKubinec <arbikej88@gmail.com>
2024-03-18 14:00:23 +0100

    styling

--- client/src/Views/Tables/TabFreeSaleMedicaments.jsx
+++ client/src/Views/Tables/TabFreeSaleMedicaments.jsx
@@ -181,11 +181,17 @@ export default function TabFreeSaleMedicaments() {
       <Dialog
         header={
           selectedRow != null ? (
-            <div>
+            <div
+              style={{
+                margin: "auto",
+                display: "flex",
+                flexDirection: "column",
+                alignItems: "center",
+              }}
+            >
               {selectedRow.NAZOV_LEKARNE}
               <br />
-              <br />
-              {selectedRow.NAZOV_LIEKU}
+              <h5>{selectedRow.NAZOV_LIEKU}</h5>
             </div>
           ) : (
             ""
