5d0351fcac1057792e9185e01ad0b1f4e649cfd9
RobertKubinec <arbikej88@gmail.com>
2024-04-02 19:07:28 +0200

    oprava

--- server/routes/warehouseTransfersRoute.js
+++ server/routes/warehouseTransfersRoute.js
@@ -4,33 +4,33 @@ const controller = require("../controllers/WarehouseTransfersController");
 const verify = require("../middleware/verifyUser");
 
 router.get(
-    "/allFin/:id",
-    verify.verifyRoles(0, 2, 3, 5),
-    controller.getFinishedTransfers
+  "/allFin/:id",
+  verify.verifyRoles(0, 2, 3, 5),
+  controller.getFinishedTransfers
 );
 
 router.get(
-    "/allWait/:id",
-    verify.verifyRoles(0, 2, 3, 5),
-    controller.getWaitingTransfers
+  "/allWait/:id",
+  verify.verifyRoles(0, 2, 3, 5),
+  controller.getWaitingTransfers
 );
 
 router.get(
-    "/allDec/:id",
-    verify.verifyRoles(0, 2, 3, 5),
-    controller.getDeclinedTransfers
+  "/allDec/:id",
+  verify.verifyRoles(0, 2, 3, 5),
+  controller.getDeclinedTransfers
 );
 
 router.get(
-    "/reqTransfers/:id",
-    verify.verifyRoles(0, 2, 3, 5),
-    controller.getRequestedTransfers
-)
+  "/reqTransfers/:id",
+  verify.verifyRoles(0, 2, 3, 5),
+  controller.getRequestedTransfers
+);
 
 router.get(
-    "/list/:id",
-    verify.verifyRoles(0, 2, 3, 5),
-    controller.getListTransfers
+  "/list/:id",
+  verify.verifyRoles(0, 2, 3, 5),
+  controller.getListTransfers
 );
 
 router.get(
@@ -40,45 +40,45 @@ router.get(
 );
 
 router.get(
-    "/hospitalMedications/:id",
-    verify.verifyRoles(0, 2, 3, 5),
-    controller.getHospitalMedication
+  "/hospitalMedications/:id",
+  verify.verifyRoles(0, 2, 3, 5),
+  controller.getHospitalMedication
 );
 
 router.get(
-    "/selectedMedications/:id/:exp_date/:usr_id",
-    verify.verifyRoles(0, 2, 3, 5),
-    controller.getSelectedMedications
-)
-
-router.post(
-    "/deleteTransfer",
-    verify.verifyRoles(0, 2, 3, 5),
-    controller.deleteTransfer
-)
-
-router.post(
-    "/createTransfer",
-    verify.verifyRoles(0, 2, 3, 5),
-    controller.createTransfer
+  "/selectedMedications/:id/:exp_date/:usr_id",
+  verify.verifyRoles(0, 2, 3, 5),
+  controller.getSelectedMedications
 );
 
 router.post(
-    "/deniedTransfer",
-    verify.verifyRoles(0, 2, 3, 5),
-    controller.deniedTransfer
+  "/deleteTransfer",
+  verify.verifyRoles(0, 2, 3, 5),
+  controller.deleteTransfer
 );
 
 router.post(
-    "/confirmTransfer",
-    verify.verifyRoles(0, 2, 3, 5),
-    controller.confirmTransfer
+  "/createTransfer",
+  verify.verifyRoles(0, 2, 3, 5),
+  controller.createTransfer
+);
+
+router.post(
+  "/deniedTransfer",
+  verify.verifyRoles(0, 2, 3, 5),
+  controller.deniedTransfer
+);
+
+router.post(
+  "/confirmTransfer",
+  verify.verifyRoles(0, 2, 3, 5),
+  controller.confirmTransfer
 );
 
 router.post(
   "/createTransferSelMedAmount",
-  verify.verifyRoles(0, 2, 3, 5)
+  verify.verifyRoles(0, 2, 3, 5),
   controller.createTransferSelMedAmount
-)
+);
 
 module.exports = router;
